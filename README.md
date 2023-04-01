b216721bdec80e94c13f42ae44c7d40bcca827c4
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-04-01 13:14:23 -0400

    1:47:40

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # React Learning
 
-Learning React 18 with Redux
+Learning React 18 with Redux using [this](https://www.youtube.com/watch?v=2-crBg6wpp0).
+
+Disregard commit messages as they are used to keep track of the video time.
 
 ## Requirements
 
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,10 +1,41 @@
+import './index.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-	return <h1>hello world</h1>;
-}
+const BookList = () => {
+	return (
+		<section className='booklist'>
+			<Book />
+			<Book />
+			<Book />
+		</section>
+	);
+};
+
+const Book = () => {
+	return (
+		<article className='book'>
+			<Image />
+			<Title />
+			<Author />
+		</article>
+	);
+};
+
+const Image = () => (
+	<img
+		src="https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg"
+		alt="Dog Man: Twenty Thousand Fleas Under the Sea: A Graphic Novel (Dog Man 11): From the Creator of Captain Underpants"
+	/>
+);
+const Title = () => (
+	<h2>
+		Dog Man: Twenty Thousand Fleas Under the Sea: A Graphic Novel (Dog Man 11):
+		From the Creator of Captain Underpants
+	</h2>
+);
+const Author = () => <h4>Dav Pilkey</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
